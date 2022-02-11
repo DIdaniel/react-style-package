b09a84c4895cf36da23e38d30b8f50e18039338f
KIM DONG IL <dongil.kim@powertask.biz>
2022-02-11 19:34:38 +0900

    accordion

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openToggle = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => openToggle()}>
+          {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {isOpen && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
